ee36609627e796bb0027c8d4ab257c5624a58bfe
DanielGu23 <daniel.gu23@ycdsbk12.ca>
2022-11-08 10:11:47 -0500

    getName() passenger class

--- src/Airplane/Passenger.java
+++ src/Airplane/Passenger.java
@@ -14,20 +14,29 @@ public class Passenger {
     private boolean hasFlightExperience;
     private String strTripPurpose;
 
- /**
-  * Constructor - creates new instance of a passanger object
-  *
-  * @param strName - the name of the passenger
-  * @param intAge - the age of the passenger
-  * @param hasFlightExperience - determines if the passenger has former flight experience
-  * @param strTripPurpose - describes the purpose of the passanger flying on the airplane
-  */	
+   /**
+    * Constructor - creates new instance of a passanger object
+    *
+    * @param strName - the name of the passenger
+    * @param intAge - the age of the passenger
+    * @param hasFlightExperience - determines if the passenger has former flight experience
+    * @param strTripPurpose - describes the purpose of the passanger flying on the airplane
+    */	
     public Passenger (String strName, int intAge, boolean hasFlightExperience, String strTripPurpose) {
     
         this.strName = strName;
         this.intAge = intAge;
         this.hasFlightExperience = hasFlightExperience;
         this.strTripPurpose = strTripPurpose;
-
 	}
+
+   /**
+    * Returns the name of the passenger
+    *
+    * @return String strName, the name of the passenger
+    */	
+    public String getName() {
+        return this.strName;
+    }
+
 }
