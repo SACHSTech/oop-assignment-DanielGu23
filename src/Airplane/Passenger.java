428fc1a7a35baf311144dc3e8c74bf8ee76f5e19
DanielGu23 <daniel.gu23@ycdsbk12.ca>
2022-11-08 10:01:59 -0500

    Initalized some classes

---
+++ src/Airplane/Passenger.java
@@ -0,0 +1,33 @@
+package Airplane;
+
+/**
+* Passenger class file
+* @author: D. Gu
+* 
+*/
+
+public class Passenger {
+
+    // Instance variables
+    private String strName;
+    private int intAge;
+    private boolean hasFlightExperience;
+    private String strTripPurpose;
+
+ /**
+  * Constructor - creates new instance of a passanger object
+  *
+  * @param strName - the name of the passenger
+  * @param intAge - the age of the passenger
+  * @param hasFlightExperience - determines if the passenger has former flight experience
+  * @param strTripPurpose - describes the purpose of the passanger flying on the airplane
+  */	
+    public Passenger (String strName, int intAge, boolean hasFlightExperience, String strTripPurpose) {
+    
+        this.strName = strName;
+        this.intAge = intAge;
+        this.hasFlightExperience = hasFlightExperience;
+        this.strTripPurpose = strTripPurpose;
+
+	}
+}
