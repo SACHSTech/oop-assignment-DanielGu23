ca5dd58187ce0365808ccb07570a76e00c50aad4
DanielGu23 <daniel.gu23@ycdsbk12.ca>
2022-11-11 13:05:49 -0500

    Boeing737 class

--- src/Airplane/Boeing737.java
+++ src/Airplane/Boeing737.java
@@ -1,20 +1,18 @@
 package Airplane;
 
-public class Helicopter extends Airplane{
-
+public class Boeing737 extends Airplane {
+   
     /**
-    * Constructor - creates new instance of a helicopter object
+    * Constructor - creates new instance of a Boeing737 object
     *
     * @param dblDistance - the distance of the flight
     * @param strStartLocation - the name of the starting location
     * @param strAirlineName - the name of the airline
     * @param intVelocity - the velocity at which the plane will be flying at
     */	
-    public Helicopter(double dblDistance, String strStartLocation, String strAirlineName, int intVelocity){
+    public Boeing737(double dblDistance, String strStartLocation, String strAirlineName, int intVelocity){
         
         // Super method to take in parameters from Airplane (parent) class
         super(dblDistance, strStartLocation, strAirlineName, intVelocity);
     }
-
-
 }
