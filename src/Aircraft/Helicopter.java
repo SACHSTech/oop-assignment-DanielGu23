2bb90ecad779c0bbd7e3f4995fce8a7eefa74a46
DanielGu23 <daniel.gu23@ycdsbk12.ca>
2022-11-15 09:26:21 -0500

    getAltitude()

--- src/Aircraft/Helicopter.java
+++ src/Aircraft/Helicopter.java
@@ -1,6 +1,9 @@
 package Aircraft;
 
 public class Helicopter extends Aircraft{
+   
+    // Instance variables
+    int intAltitude;
 
     /**
     * Constructor - creates new instance of a helicopter object
@@ -10,9 +13,19 @@ public class Helicopter extends Aircraft{
     * @param strAirlineName - the name of the airline
     * @param intVelocity - the velocity at which the plane will be flying at
     */	
-    public Helicopter(double dblDistance, String strStartLocation, String strAirlineName, int intVelocity, double dblBladeLength){
+    public Helicopter(double dblDistance, String strStartLocation, String strAirlineName, int intVelocity, int intAltitude){
         
         // Super method to take in parameters from Airplane (parent) class
         super(dblDistance, strStartLocation, strAirlineName, intVelocity);
+        this.intAltitude = intAltitude;
+    }
+
+    /**
+    * Returns the altitude the helicopter will aim to fly at
+    *
+    * @return intAltitude, the altitude of the helicopter
+    */	
+    public int getAltitude(){
+        return this.intAltitude;
     }
 }
